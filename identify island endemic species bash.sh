60e4a8337be7a9e9d172797df06477b0c5cead7d
fenglu0608-max <fenglu0608@gmail.com>
2026-06-10 17:24:16 +0200

    Update SLURM job parameters and script execution

--- identify island endemic species bash.sh
+++ identify island endemic species bash.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#SBATCH -p regular
-#SBATCH --time=04:00:00
+#SBATCH --job-name=global_is
+#SBATCH --partition=regularmedium
+#SBATCH --time=56:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --job-name=island_all_fix
-#SBATCH --output=island_all_fix_%A_%a.out
-#SBATCH --error=island_all_fix_%A_%a.err
-#SBATCH --array=17-150%5
-#SBATCH --mem=32GB
+#SBATCH --mem=64G
+#SBATCH --array=1-120%8
+#SBATCH --output=logs/global_island_%A_%a.out
+#SBATCH --error=logs/global_island_%A_%a.err
 
-cd ~/island_bird_project
+set -euo pipefail
 
-module --force purge
+cd "$HOME/island_bird_project"
+mkdir -p logs
+
+export SPECIES_PER_JOB=100
+export CHUNK_SIZE=10
 
 RSCRIPT="/cvmfs/hpc.rug.nl/versions/2023.01/rocky8/x86_64/amd/zen3/software/R/4.2.1-foss-2022a/bin/Rscript"
 
-export SPECIES_PER_JOB=100
-
-$RSCRIPT island_area_all_species_recalc.R
+"$RSCRIPT" island_bird_global_island_filter_stratified.R
